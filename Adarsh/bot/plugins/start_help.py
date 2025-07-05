556cd93ebc79b1f94ffceea11ca564d20f8b14a6
God2020k <107332649+God2020k@users.noreply.github.com>
2025-07-05 08:35:47 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -2,7 +2,7 @@
 from Adarsh.bot import StreamBot
 from Adarsh.vars import Var
 import logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(name)
 from Adarsh.bot.plugins.stream import MY_PASS
 from Adarsh.utils.human_readable import humanbytes
 from Adarsh.utils.database import Database
@@ -11,36 +11,50 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
 from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
-from pyrogram.types import ReplyKeyboardMarkup
+
+# Removed ReplyKeyboardMarkup import since we use inline keyboards now
 
 if MY_PASS:
-            buttonz=ReplyKeyboardMarkup(
+    buttonz = InlineKeyboardMarkup(
+        [
             [
-                ["start⚡️","help📚","login🔑","DC"],
-                ["Channel❤️","ping📡","status📊","Owner😎"]
-                        
+                InlineKeyboardButton("start⚡️", callback_data="start"),
+                InlineKeyboardButton("help📚", callback_data="help"),
+                InlineKeyboardButton("login🔑", callback_data="login"),
+                InlineKeyboardButton("DC", callback_data="dc"),
             ],
-            resize_keyboard=True
-        )
+            [
+                InlineKeyboardButton("Channel❤️", callback_data="channel"),
+                InlineKeyboardButton("ping📡", callback_data="ping"),
+                InlineKeyboardButton("status📊", callback_data="status"),
+                InlineKeyboardButton("Owner😎", callback_data="owner"),
+            ],
+        ]
+    )
 else:
-            buttonz=ReplyKeyboardMarkup(
+    buttonz = InlineKeyboardMarkup(
+        [
             [
-                ["start⚡️","help📚","DC"],
-                ["Channel❤️","ping📡","status📊","Owner😎"]
-                        
+                InlineKeyboardButton("start⚡️", callback_data="start"),
+                InlineKeyboardButton("help📚", callback_data="help"),
+                InlineKeyboardButton("DC", callback_data="dc"),
             ],
-            resize_keyboard=True
-        )
+            [
+                InlineKeyboardButton("Channel❤️", callback_data="channel"),
+                InlineKeyboardButton("ping📡", callback_data="ping"),
+                InlineKeyboardButton("status📊", callback_data="status"),
+                InlineKeyboardButton("Owner😎", callback_data="owner"),
+            ],
+        ]
+    )
 
-            
-            
-@StreamBot.on_message((filters.command("start") | filters.regex('start⚡️')) & filters.private )
+@StreamBot.on_message((filters.command("start") | filters.regex('start⚡️')) & filters.private)
 async def start(b, m):
     if not await db.is_user_exist(m.from_user.id):
         await db.add_user(m.from_user.id)
         await b.send_message(
             Var.BIN_CHANNEL,
-            f"**Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ:** \n\n__Mʏ Nᴇᴡ Fʀɪᴇɴᴅ__ [{m.from_user.first_name}](tg://user?id={m.from_user.id}) __Sᴛᴀʀᴛᴇᴅ Yᴏᴜʀ Bᴏᴛ !!__"
+            f"Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ: \n\nMʏ Nᴇᴡ Fʀɪᴇɴᴅ [{m.from_user.first_name}](tg://user?id={m.from_user.id}) Sᴛᴀʀᴛᴇᴅ Yᴏᴜʀ Bᴏᴛ !!"
         )
     if Var.UPDATES_CHANNEL != "None":
         try:
@@ -48,12 +62,12 @@ async def start(b, m):
             if user.status == "kicked":
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="__𝓢𝓞𝓡𝓡𝓨, 𝓨𝓞𝓤 𝓐𝓡𝓔 𝓐𝓡𝓔 𝓑𝓐𝓝𝓝𝓔𝓓 𝓕𝓡𝓞𝓜 𝓤𝓢𝓘𝓝𝓖 𝓜𝓔. 𝓒ᴏɴᴛᴀᴄᴛ ᴛʜᴇ 𝓓ᴇᴠᴇʟᴏᴘᴇʀ__\n\n  **𝙃𝙚 𝙬𝙞𝙡𝙡 𝙝𝙚𝙡𝙥 𝙮𝙤𝙪**",
+                    text="𝓢𝓞𝓡𝓡𝓨, 𝓨𝓞𝓤 𝓐𝓡𝓔 𝓐𝓡𝓔 𝓑𝓐𝓝𝓝𝓔𝓓 𝓕𝓡𝓞𝓜 𝓤𝓢𝓘𝓝𝓖 𝓜𝓔. 𝓒ᴏɴᴛᴀᴄᴛ ᴛʜᴇ 𝓓ᴇᴠᴇʟᴏᴘᴇʀ\n\n  𝙃𝙚 𝙬𝙞𝙡𝙡 𝙝𝙚𝙡𝙥 𝙮𝙤𝙪",
                     disable_web_page_preview=True
                 )
                 return
         except UserNotParticipant:
-             await StreamBot.send_photo(
+            await StreamBot.send_photo(
                 chat_id=m.chat.id,
                 photo="https://graph.org/file/4b8bf6ec079dbe5aac0cf.jpg",
                 caption="<i>𝙹𝙾𝙸𝙽 𝚃𝙷𝙸𝚂 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝚃𝙾 𝚄𝚂𝙴 𝙼𝙴🔐</i>",
@@ -64,30 +78,30 @@ async def start(b, m):
                         ]
                     ]
                 ),
-                
             )
-             return
+            return
         except Exception:
             await b.send_message(
                 chat_id=m.chat.id,
                 text="<i>𝓢𝓸𝓶𝓮𝓽𝓱𝓲𝓷𝓰 𝔀𝓮𝓷𝓽 𝔀𝓻𝓸𝓷𝓰</i> <b> <a href='https://t.me/SAM_DUB_LEZHa'>CLICK HERE FOR SUPPORT </a></b>",
-                
-                disable_web_page_preview=True)
+                disable_web_page_preview=True
+            )
             return
     await StreamBot.send_photo(
         chat_id=m.chat.id,
-        photo ="https://graph.org/file/4b8bf6ec079dbe5aac0cf.jpg",
-        caption =f'𝐇𝐢𝐢 {m.from_user.mention(style="md")}...!,\n𝐈 𝐀𝐦 𝐒𝐌𝐃 𝐅𝐢𝐥𝐞 𝐓𝐨 𝐋𝐢𝐧𝐤 𝐑𝐨𝐁𝐨𝐭 𝐅𝐨𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐎𝐰𝐧𝐞𝐫𝐬 𝐨𝐧𝐥𝐲.\n𝐒𝐞𝐧𝐝 𝐌𝐞 𝐚𝐧𝐲 𝐅𝐢𝐥𝐞 𝐚𝐧𝐝 𝐠𝐞𝐭 𝐚 𝐃𝐢𝐫𝐞𝐜𝐭 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐋𝐢𝐧𝐤 𝐚𝐧𝐝 𝐒𝐭𝐫𝐞𝐚𝐦𝐞𝐛𝐥𝐞 𝐋𝐢𝐧𝐤.!',
-        reply_markup=buttonz)
+            photo="https://graph.org/file/4b8bf6ec079dbe5aac0cf.jpg",
+        caption=f'𝐇𝐢𝐢 {m.from_user.mention(style="md")}...!,\n𝐈 𝐀𝐦 𝐒𝐌𝐃 𝐅𝐢𝐥𝐞 𝐓𝐨 𝐋𝐢𝐧𝐤 𝐑𝐨𝐁𝐨𝐭 𝐅𝐨𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐎𝐰𝐧𝐞𝐫𝐬 𝐨𝐧𝐥𝐲.\n𝐒𝐞𝐧𝐝 𝐌𝐞 𝐚𝐧𝐲 𝐅𝐢𝐥𝐞 𝐚𝐧𝐝 𝐠𝐞𝐭 𝐚 𝐃𝐢𝐫𝐞𝐜𝐭 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐋𝐢𝐧𝐤 𝐚𝐧𝐝 𝐒𝐭𝐫𝐞𝐚𝐦𝐛𝐥𝐞 𝐋𝐢𝐧𝐤.!',
+        reply_markup=buttonz
+    )
 
 
-@StreamBot.on_message((filters.command("help") | filters.regex('help📚')) & filters.private )
+@StreamBot.on_message((filters.command("help") | filters.regex('help📚')) & filters.private)
 async def help_handler(bot, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id)
         await bot.send_message(
             Var.BIN_CHANNEL,
-            f"**Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ **\n\n__Mʏ Nᴇᴡ Fʀɪᴇɴᴅ__ [{message.from_user.first_name}](tg://user?id={message.from_user.id}) __Started Your Bot !!__"
+            f"Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ \n\nMʏ Nᴇᴡ Fʀɪᴇɴᴅ [{message.from_user.first_name}](tg://user?id={message.from_user.id}) Started Your Bot !!"
         )
     if Var.UPDATES_CHANNEL != "None":
         try:
@@ -96,7 +110,6 @@ async def help_handler(bot, message):
                 await bot.send_message(
                     chat_id=message.chat.id,
                     text="<i>Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ FROM USING ᴍᴇ. Cᴏɴᴛᴀᴄᴛ ᴛʜᴇ Dᴇᴠᴇʟᴏᴘᴇʀ</i>",
-                    
                     disable_web_page_preview=True
                 )
                 return
@@ -104,7 +117,7 @@ async def help_handler(bot, message):
             await StreamBot.send_photo(
                 chat_id=message.chat.id,
                 photo="https://graph.org/file/4b8bf6ec079dbe5aac0cf.jpg",
-                Caption="**𝙹𝙾𝙸𝙽 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿 𝚃𝙾 𝚄𝚂𝙴 ᴛʜɪs Bᴏᴛ!**\n\n__Dᴜᴇ ᴛᴏ Oᴠᴇʀʟᴏᴀᴅ, Oɴʟʏ Cʜᴀɴɴᴇʟ Sᴜʙsᴄʀɪʙᴇʀs ᴄᴀɴ ᴜsᴇ ᴛʜᴇ Bᴏᴛ!__",
+                caption="𝙹𝙾𝙸𝙽 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿 𝚃𝙾 𝚄𝚂𝙴 ᴛʜɪs Bᴏᴛ!\n\nDᴜᴇ ᴛᴏ Oᴠᴇʀʟᴏᴀᴅ, Oɴʟʏ Cʜᴀɴɴᴇʟ Sᴜʙsᴄʀɪʙᴇʀs ᴄᴀɴ ᴜsᴇ ᴛʜᴇ Bᴏᴛ!",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
@@ -112,24 +125,29 @@ async def help_handler(bot, message):
                         ]
                     ]
                 ),
-                
             )
             return
         except Exception:
             await bot.send_message(
                 chat_id=message.chat.id,
-                text="__Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍᴇ__ [🦋𝐒𝐌𝐃_𝐎𝐰𝐧𝐞𝐫🍁](https://t.me/SMD_Owner).",
-                disable_web_page_preview=True)
+                text="Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍᴇ [🦋𝐒𝐌𝐃_𝐎𝐰𝐧𝐞𝐫🍁](https://t.me/SMD_Owner).",
+                disable_web_page_preview=True
+            )
             return
     await message.reply_text(
-        text="""<b> Send me any file or video i will give you streamable link and download link.</b>\n
-<b> I also support Channels, add me to you Channel and send any media files and see miracle✨ also send /list to know all commands""",
-        
+        text=(
+            "<b>Send me any file or video and I will give you streamable and download links.</b>\n"
+            "<b>I also support Channels! Add me to your Channel and send media files to see magic ✨. Use /list to see all commands.</b>"
+        ),
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("🦋𝐒𝐌𝐃_𝐎𝐰𝐧𝐞𝐫🍁", url="https://t.me/SMD_Owner")],
-                [InlineKeyboardButton("🦋𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞🍁", url="https://t.me/SMD_BOTz")]
+                [
+                    InlineKeyboardButton("🦋𝐒𝐌𝐃_𝐎𝐰𝐧𝐞𝐫🍁", url="https://t.me/SMD_Owner")
+                ],
+                [
+                    InlineKeyboardButton("🦋𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞🍁", url="https://t.me/SMD_BOTz")
+                ]
             ]
-        )
-    )
+        ),
+            )
