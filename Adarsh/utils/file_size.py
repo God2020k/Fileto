e32316d70e87aa7961b6bd163d7c330f037fb5bb
God2020k <107332649+God2020k@users.noreply.github.com>
2025-07-05 09:04:55 +0530

    Update file_size.py

--- Adarsh/utils/file_size.py
+++ Adarsh/utils/file_size.py
@@ -1,4 +1,26 @@
-# (c) adarsh-goel
-def human_size(bytes, units=[' bytes','KB','MB','GB','TB', 'PB', 'EB']):
-    """ Returns a human readable string representation of bytes """
-    return str(bytes) + units[0] if int(bytes) < 1024 else human_size(int(bytes)>>10, units[1:])
+def get_readable_file_size(size: int) -> str:
+    # Converts bytes to human-readable string
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if size < 1024:
+            return f"{size:.2f} {unit}"
+        size /= 1024
+    return f"{size:.2f} PB"
+
+def readable_time(seconds: int) -> str:
+    # Converts seconds to human-readable uptime string
+    count = 0
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "d"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and result == 0:
+            break
+        time_list.append(f"{int(result)}{time_suffix_list[count - 1]}")
+        seconds = int(remainder)
+
+    return ":".join(reversed(time_list))
